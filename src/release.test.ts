9a37794695b1d0e886ecae0c38bcd7b028d4753e
Anton Malinskiy <anton@malinskiy.com>
2024-02-07 15:04:52 +1000

    chore(build): build

--- src/release.test.ts
+++ src/release.test.ts
@@ -1,8 +1,24 @@
-import { getReleaseArtifact } from "./release";
+import { getReleaseArtifact } from './release';
 
 test('getReleaseArtifact', () => {
-  expect(getReleaseArtifact("1.0.0", 'universal-apple-darwin').artifactUrl).toBe('https://github.com/MarathonLabs/marathon-cloud-cli/releases/download/1.0.0/marathon-cloud-v1.0.0-universal-apple-darwin.tar.gz')
-  expect(getReleaseArtifact("1.0.0", 'x86_64-unknown-linux-gnu').artifactUrl).toBe('https://github.com/MarathonLabs/marathon-cloud-cli/releases/download/1.0.0/marathon-cloud-v1.0.0-x86_64-unknown-linux-gnu.tar.gz')
-  expect(getReleaseArtifact("1.0.0", 'aarch64-unknown-linux-gnu').artifactUrl).toBe('https://github.com/MarathonLabs/marathon-cloud-cli/releases/download/1.0.0/marathon-cloud-v1.0.0-aarch64-unknown-linux-gnu.tar.gz')
-  expect(getReleaseArtifact("1.0.0", 'x86_64-pc-windows-msvc').artifactUrl).toBe('https://github.com/MarathonLabs/marathon-cloud-cli/releases/download/1.0.0/marathon-cloud-v1.0.0-x86_64-pc-windows-msvc.zip')
+  expect(
+    getReleaseArtifact('1.0.0', 'universal-apple-darwin').artifactUrl,
+  ).toBe(
+    'https://github.com/MarathonLabs/marathon-cloud-cli/releases/download/1.0.0/marathon-cloud-v1.0.0-universal-apple-darwin.tar.gz',
+  );
+  expect(
+    getReleaseArtifact('1.0.0', 'x86_64-unknown-linux-gnu').artifactUrl,
+  ).toBe(
+    'https://github.com/MarathonLabs/marathon-cloud-cli/releases/download/1.0.0/marathon-cloud-v1.0.0-x86_64-unknown-linux-gnu.tar.gz',
+  );
+  expect(
+    getReleaseArtifact('1.0.0', 'aarch64-unknown-linux-gnu').artifactUrl,
+  ).toBe(
+    'https://github.com/MarathonLabs/marathon-cloud-cli/releases/download/1.0.0/marathon-cloud-v1.0.0-aarch64-unknown-linux-gnu.tar.gz',
+  );
+  expect(
+    getReleaseArtifact('1.0.0', 'x86_64-pc-windows-msvc').artifactUrl,
+  ).toBe(
+    'https://github.com/MarathonLabs/marathon-cloud-cli/releases/download/1.0.0/marathon-cloud-v1.0.0-x86_64-pc-windows-msvc.zip',
+  );
 });
